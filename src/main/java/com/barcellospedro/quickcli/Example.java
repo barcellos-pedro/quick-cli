80601c045cdebaafffe9bf841dddd88655b56adf
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-02-18 22:15:45 -0300

    Create FilesHelper and update DTO command

--- src/main/java/com/barcellospedro/quickcli/Example.java
+++ src/main/java/com/barcellospedro/quickcli/Example.java
@@ -3,7 +3,11 @@ package com.barcellospedro.quickcli;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
 
-@Command
+import java.io.IOException;
+
+import static java.text.MessageFormat.format;
+
+@Command(command = "quick generate")
 class Example {
     @Command(command = "greet", description = "Displays a welcoming message.")
     String greet(@Option(description = "Tell your name. E.g: --name John Doe",
@@ -16,4 +20,28 @@ class Example {
     String example(@Option(longNames = "person", shortNames = 'p', required = true) String person) {
         return "Hello %s.".formatted(person);
     }
+
+    @Command(command = "dto")
+    String createDTO(
+            @Option(required = true, shortNames = 'n', longNames = "name") String className,
+            @Option(shortNames = 'r', longNames = "record", defaultValue = "false") Boolean useRecord
+    ) {
+        try (var file = FilesHelper.getWriter(className)) {
+            file.write(content(className));
+            return "DTO created!";
+        } catch (IOException exception) {
+            return exception.getMessage();
+        }
+    }
+
+    private static String content(String className) {
+        // TOOD: Check for boolean flag to use Records or POJOs
+        return format("""
+                package {0};
+                
+                record {1}(String name) '{}'
+                """, FilesHelper.rootPackage(), className);
+    }
+
+
 }
---
+++ src/main/java/com/barcellospedro/quickcli/FilesHelper.java
@@ -0,0 +1,30 @@
+package com.barcellospedro.quickcli;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.text.MessageFormat.format;
+
+public class FilesHelper {
+    public static BufferedWriter getWriter(String file) throws IOException {
+        Path path = Paths.get(fileName(file));
+        return Files.newBufferedWriter(path);
+    }
+
+    public static String rootDir() {
+        return System.getProperty("user.dir");
+    }
+
+    public static String rootPackage() {
+        return QuickCliApplication.class.getPackage().getName();
+    }
+
+    public static String fileName(String className) {
+        String pkg = rootPackage().replace(".", File.separator);
+        return format("src/main/java/{0}/{1}.java", pkg, className);
+    }
+}
